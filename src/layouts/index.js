a3a7fd4a1cb6fa47bc322f73269ccbcc68225956
Alestry Pérez <alestry.perez@hyperisland.se>
2021-01-19 21:33:58 +0100

    commit

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -71,7 +71,14 @@ class TemplateWrapper extends Component {
           onSwipedRight={this.swipeRight}
         >
           <Transition location={location}>
-            <div id="slide" style={{'width': '100%'}}>{children}</div>
+            <div
+              id="slide"
+              style={{
+                width: '100%',
+              }}
+            >
+              {children}
+            </div>
           </Transition>
         </Swipeable>
       </div>
